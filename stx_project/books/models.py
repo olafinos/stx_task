2fbb647905e92fdf96ea80b86d4147028efb91f7
olafinos <olafinos@o2.pl>
2021-01-02 14:01:53 +0100

    Added Book model

--- stx_project/books/models.py
+++ stx_project/books/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    isbn_number = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=150)
+    author = models.CharField(max_length=150)
+    publication_date = models.DateField()
+    number_of_pages = models.IntegerField()
+    cover_link = models.CharField(max_length=200)
+    publication_language = models.CharField(max_length=150)
